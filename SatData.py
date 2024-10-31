94e78e2aa78c5046718f37ddfd194c374df5265d
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-10-31 16:37:43 -0700

    SatData.py

--- SatData.py
+++ SatData.py
@@ -12,17 +12,28 @@ class SatData:
         """
         Initialize SATData class by loading SAT data from 'sat.json'.
         """
-        with open("sat.json", "r") as file:
+        with open('sat.json', 'r') as file:
                 self._data = json.load(file)
 
-    def save_as_csv(self, dbns, output_filename="output.csv"):
+    def save_as_csv(self, dbns):
         """
-        Save SAT data to CSV file named 'output.csv'.
+        Save SAT data to CSV file.
         """
-        dbns.sort()
-        with open(output_filename, "w", newline='') as csv_file:
-            csv_file.write("DBN, School Name, Number of Test Takers, Critical Reading Mean, Mathematics Mean, Writing Mean\n")
-            for row in self._data['school_data']:
-                if row['DBN'] in dbns:
-                    school_name = row['school_name'].replace(",", r'\"')
-                    csv_file.write(f"{row['DBN']}, {school_name}, {row['num_tested']}, {row['critical_reading_mean']}, {row['mathematics_mean']}, {row['writing_mean']}\n")
+        dbns = sorted(dbns)
+
+        headers = ["DBN", "School Name", "Number of Test Takers", "Critical Reading Mean", "Mathematics Mean",
+                   "Writing Mean"]
+
+        with open('output.csv', 'w') as file:
+            file.write(",".join(headers) + "\n")
+            for dbn in dbns:
+                school_data = next((school for school in self._data if school["DBN"] == dbn), None)
+                if school_data:
+                    row = [
+                        school_data.get["DBN", ""],
+                        f'"{school_data.get("School Name", "")}"' if "," in school_data.get("School Name", "") else school_data.get("School Name", ""),
+                        school_data.get("Number of Test Takers", ""),
+                        school_data.get("Critical Reading Mean", ""),
+                        school_data.get("Mathematics Mean", "")
+                    ]
+                    file.write(','.join(str(value) if value != "" else "" for value in row) + '\n')
