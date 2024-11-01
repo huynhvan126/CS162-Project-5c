38fe7d9dd875dc50dc8cb58a72b618e87351fc5f
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-10-31 17:27:02 -0700

    SatData.py

--- SatData.py
+++ SatData.py
@@ -4,9 +4,6 @@
 # Description: Write a class named Satdata that read JSON file and a data to a text file in CSv format.
 import json
 
-def str(value):
-    pass
-    
 class SatData:
     """
     A class to handle SAT data, reading SAT data for NYC from JSON file and saving specific school data to a CSV file.
@@ -15,28 +12,29 @@ class SatData:
         """
         Initialize SATData class by loading SAT data from 'sat.json'.
         """
-        with open('sat.json', 'r') as file:
-                self._data = json.load(file)
+        try:
+            with open('sat.json', 'r') as json_file:
+                self._data = json.load(json_file)
+            except FileNotFoundError:
+                print('file not found')
 
-    def save_as_csv(self, dbns):
+    def save_as_csv(self, dbn_list):
         """
         Save SAT data to CSV file.
         """
-        dbns = sorted(dbns)
-
+        csv_list = []
         headers = ["DBN", "School Name", "Number of Test Takers", "Critical Reading Mean", "Mathematics Mean",
                    "Writing Mean"]
-
-        with open('output.csv', 'w') as file:
-            file.write(",".join(headers) + "\n")
-            for dbn in dbns:
-                school_data = next((school for school in self._data if school["DBN"] == dbn), None)
-                if school_data:
-                    row = [
-                        school_data.get("DBN", ""),
-                        f'"{school_data.get("School Name", "")}"' if "," in school_data.get("School Name", "") else school_data.get("School Name", ""),
-                        school_data.get("Number of Test Takers", ""),
-                        school_data.get("Critical Reading Mean", ""),
-                        school_data.get("Mathematics Mean", "")
-                    ]
-                    file.write(','.join(str(value) if value != "" else "" for value in row) + '\n')
+        for item in self._data['data']:
+            if item[8] in dbn_list:
+                csv_list.append(item[8:14])
+        sorted(csv_list)
+        with open('output.csv', 'w') as new_file:
+            new_file.write(','.join(headers))
+            new_file.write('\n')
+            pos = 0
+            while pos < len(csv_list):
+                new_file.write(csv_list[pos])
+                new_file.write('\n')
+                pos += 1
+            return csv_list
